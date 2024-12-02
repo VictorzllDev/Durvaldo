0d64158c5c646154d83658b999f1eaace7aaf937
VictorzllDev <VictorzllDev@gmail.com>
2024-12-02 04:12:59 -0300

    add cors origin *

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,12 @@
 import fastify from 'fastify'
+import fastifyCors from '@fastify/cors'
 import { productRoutes } from './routes/product.routes'
 
 const app = fastify()
+
+app.register(fastifyCors, {
+	origin: '*',
+})
 
 app.register(productRoutes, {
 	prefix: '/product',
