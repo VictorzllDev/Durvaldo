f5f898fa322386a9493a75805a9e1f0f385fdccb
VictorzllDev <VictorzllDev@gmail.com>
2024-12-03 14:21:59 -0300

    add env

--- frontend/src/services/apiDefault.ts
+++ frontend/src/services/apiDefault.ts
@@ -2,5 +2,5 @@ import axios from 'axios'
 
 // Criação da instância do axios com a baseURL
 export const apiDefault = axios.create({
-	baseURL: 'http://localhost:3333',
+	baseURL: import.meta.env.VITE_API,
 })
